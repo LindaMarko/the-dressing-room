dd58b1548e7578fb2e12fe44fa188629c47af1be
Linda Marko <lindamarko.dev@gmail.com>
2023-04-28 09:42:04 +0200

    Create Layout Component

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,5 @@
-import { Inter } from 'next/font/google';
-
-const inter = Inter({ subsets: ['latin'] });
+import Layout from '@/components/Layout';
 
 export default function Home() {
-  return (
-    <div>
-      <h1 className="text-3xl font-bold">Next Tailwind</h1>
-    </div>
-  );
+  return <Layout>Home Page</Layout>;
 }
