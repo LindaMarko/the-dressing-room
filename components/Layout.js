3796c0b2d96ba186a8eafb9fa6dc4d70c9eac668
Linda Marko <lindamarko.dev@gmail.com>
2023-04-30 10:17:13 +0200

    Create StoreProvider, handle Add to cart

--- components/Layout.js
+++ components/Layout.js
@@ -1,8 +1,12 @@
 import Head from 'next/head';
 import Link from 'next/link';
-import React from 'react';
+import React, { useContext } from 'react';
+import { Store } from '@/utils/Store';
 
 export default function Layout({ title, children }) {
+  const { state } = useContext(Store);
+  const { cart } = state;
+
   return (
     <>
       <Head>
@@ -21,6 +25,11 @@ export default function Layout({ title, children }) {
             <div>
               <Link href="/cart" className="p-2">
                 Cart
+                {cart.cartItems.length > 0 && (
+                  <span className="ml-1 rounded-full bg-red-600 px-2 py-1 text-xs font-bold text-white">
+                    {cart.cartItems.reduce((a, c) => a + c.quantity, 0)}
+                  </span>
+                )}
               </Link>
               <Link href="/login" className="p-2">
                 Login
@@ -31,7 +40,7 @@ export default function Layout({ title, children }) {
         <main className="container max-w-7xl m-auto mt-12 mb-18 px-8">
           {children}
         </main>
-        <footer className="flex h-10 justify-center items-center shadow-inner">
+        <footer className="flex h-10 justify-center items-center shadow-inner mt-40">
           <p>Copyright © 2023 The Dressing Room</p>
         </footer>
       </div>
--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,21 +1,37 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-import React from 'react';
+import React, { useContext } from 'react';
 import Layout from '../../components/Layout';
 import data from '../../utils/data';
+import { Store } from '../../utils/Store';
 
 export default function ProductScreen() {
+  const { state, dispatch } = useContext(Store);
+
   const { query } = useRouter();
   const { slug } = query;
   const product = data.products.find((x) => x.slug === slug);
   if (!product) {
     return <div>Produt Not Found</div>;
   }
+
+  const addToCartHandler = () => {
+    const existItem = state.cart.cartItems.find((x) => x.slug === product.slug);
+    const quantity = existItem ? existItem.quantity + 1 : 1;
+
+    if (product.countInStock < quantity) {
+      alert('Sorry. Product is out of stock');
+      return;
+    }
+
+    dispatch({ type: 'CART_ADD_ITEM', payload: { ...product, quantity } });
+  };
+
   return (
     <Layout title={product.name}>
-      <div className="py-2">
-        <Link href="/">back to products</Link>
+      <div className="py-4">
+        <Link href="/"> &#8592; Back to products</Link>
       </div>
       <div className="grid md:grid-cols-3 gap-8 md:gap-4">
         <div className="md:col-span-1">
@@ -32,12 +48,18 @@ export default function ProductScreen() {
             <li>
               <h1 className="text-lg font-semibold">{product.name}</h1>
             </li>
-            <li>Category: {product.category}</li>
-            <li>Brand: {product.brand}</li>
+            <li>
+              <em>Category</em>: {product.category}
+            </li>
+            <li>
+              <em>Brand</em>: {product.brand}
+            </li>
             <li>
               {product.rating} of {product.numReviews} reviews
             </li>
-            <li>Description: {product.description}</li>
+            <li className="mt-4">
+              <em>Description</em>: {product.description}
+            </li>
           </ul>
         </div>
         <div>
@@ -50,7 +72,12 @@ export default function ProductScreen() {
               <div>Status</div>
               <div>{product.countInStock > 0 ? 'In stock' : 'Unavailable'}</div>
             </div>
-            <button className="primary-button w-full">Add to cart</button>
+            <button
+              className="primary-button w-full"
+              onClick={addToCartHandler}
+            >
+              Add to cart
+            </button>
           </div>
         </div>
       </div>
